aad92b6fa0a8fc2ac79a7407e03209b3bd30abfa
mahdi2662 <madialkhalidi83@gmail.com>
2024-09-21 15:29:53 +0200

    aflevering 7 af

--- routes/web.php
+++ routes/web.php
@@ -1,57 +1,23 @@
 <?php
-use Illuminate\Support\Arr;
+
 use Illuminate\Support\Facades\Route;
+use App\Models\Job;
 
 
 
-Route::get('/', function () {
+Route::get('/', function  () {
     return view('home');
 });
 
-Route::get('/jobs', function ()  {
+Route::get('/jobs', function ()   {
         return view('jobs', [
-            'jobs' => [
-                [
-                    'id'    => 1,
-                    'title' => 'Director',
-                    'salary' => '$50,000'
-                ],
-                [
-                    'id'    => 2,
-                    'title' => 'Programmer',
-                    'salary' => '$10,000'
-                ],
-                [
-                    'id'    => 3,
-                    'title' => 'Teacher',
-                    'salary' => '$40,000'
-                ],
-            ]
+            'jobs' => job::all()
         ]);
     });
 
 
-Route::get('/jobs/{id}', function ($id) {
-    $jobs = [
-        [
-            'id'    => 1,
-            'title' => 'Director',
-            'salary' => '$50,000'
-        ],
-        [
-            'id'    => 2,
-            'title' => 'Programmer',
-            'salary' => '$10,000'
-        ],
-        [
-            'id'    => 3,
-            'title' => 'Teacher',
-            'salary' => '$40,000'
-        ],
-    ];
-
-    $job = Arr::first($jobs, fn($job) => $job['id'] == $id);
-
+Route::get('/jobs/{id}', function ($id)  {
+    $job =job::find($id);
 
     return view('job', ['job' => $job]);
 });
