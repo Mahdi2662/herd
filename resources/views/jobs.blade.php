735a88abf1e76d9c2fa1e6dd77c31cf48b00cafe
mahdi2662 <madialkhalidi83@gmail.com>
2024-10-21 11:54:33 +0200

    aflevering 10 helft

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -1,15 +1,15 @@
 <x-layout>
 
 <x-slot name="heading">
-        Jobs listings
-    </x-slot>
-    <ul>
-        @foreach ($jobs as $job)
-            <li>
-                <a href="/job/{{ $job['id'] }}" class="text-blue-500 hover:uderline">
+    Jobs listings
+</x-slot>
+<ul>
+    @foreach ($jobs as $job)
+        <li>
+            <a href="/job/{{ $job['id'] }}" class="text-blue-500 hover:underline">
                 <strong>{{ $job['title'] }}:</strong> Pays {{ $job['salary'] }} per year.
             </a>
-            </li>
-        @endforeach
+        </li>
+    @endforeach
 </ul>
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -3,30 +3,20 @@
 use Illuminate\Support\Facades\Route;
 use App\Models\Job;
 
-
-Route::get('/', function  () {
+Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/jobs', function ()   {
-        return view('jobs', [
-            'jobs' => Job::all()
-        ]);
-    });
+Route::get('/jobs', function () {
+    return view('jobs', [
+        'jobs' => Job::all()
+    ]);
+});
 
-
-Route::get('/job/{id}', function ($id)  {
-    $job =job::find($id);
+Route::get('/job/{id}', function ($id) {
+    $job = Job::find($id);
 
     return view('job', [
         'job' => $job
-]);
+    ]);
 });
-    
-
-
-Route::get('/contact', function () {
-    return view('contact');
-});
-
-
