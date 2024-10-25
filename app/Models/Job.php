cae7162ec07e4fc8190f7064d46a112a74caa94a
mahdi2662 <madialkhalidi83@gmail.com>
2024-10-25 14:23:38 +0200

    aflevering 12 een begin

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -8,6 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Job extends Model
 {
     use HasFactory;
+
     protected $table = 'job_listings';
     protected $fillable = ['title', 'salary'];
+
+    public function employer()
+    {
+        return $this->belongsTo(Employer::class);
+    }
 }
